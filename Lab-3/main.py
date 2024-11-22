e7efe3d83b21b131e87bc399e8a30623958bc197
talphony <117267975+talphony@users.noreply.github.com>
2024-11-22 22:56:23 +0500

    Add files via upload

---
+++ Lab-3/main.py
@@ -0,0 +1,40 @@
+class GeometricShape:
+    def __init__(self, name):
+        self.name = name
+    def area(self):
+        return 0
+
+class Rectangle(GeometricShape):
+    def __init__(self, name, width, height ):
+        super().__init__(name)
+        self.width = width
+        self.height = height
+    def area(self):
+        rectangle = self.width * self.height
+        return rectangle
+    def name(self):
+        return self.name
+
+class Circle(GeometricShape):
+    def __init__(self, name, radius):
+        super().__init__(name)
+        self.radius = radius
+    def area(self):
+        return (self.radius * self.radius) * 3.14
+
+class Rhombus(GeometricShape):
+    def __init__(self, name, diagonal1, diagonal2):
+        super().__init__(name)
+        self.diagonal1 = diagonal1
+        self.diagonal2 = diagonal2
+    def area(self):
+        return self.diagonal1 * self.diagonal2 * 0.5
+
+rec = Rectangle("Прямоугольник",3,8)
+print(rec.name, "\n", "Площадь:", rec.area(), "\n")
+
+cir = Circle("Круг",6)
+print(cir.name, "\n", "Площадь:", cir.area(), "\n")
+
+Rh = Rhombus("Ромб",5, 6)
+print(Rh.name, "\n", "Площадь:", Rh.area(), "\n")
